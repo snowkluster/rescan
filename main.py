439542f4dbdcfe9c44af04390c91d2886e21cc2c
Rohan Kaushik <rohankaushik1@outlook.com>
2023-07-23 23:55:22 +0530

    add basic typer interface

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+#!/usr/bin/env python3
 
 import typer
+import dns.resolver
+import dns.reversename
+import dns.zone
+import dns.exception
+import threading
+import socket
+
+app = typer.Typer()
+
+@app.command()
+def scan(ip: str):
+    print(f"scanning ip address: str({ip})")
+
+@app.command()
+def delete():
+    print("Deleting user: Hiro Hamada")
+
+if __name__ == "__main__":
+    app()
